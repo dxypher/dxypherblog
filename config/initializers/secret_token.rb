fbc9e052f2fa46e060184c65417f2fa21f5cd413
Nikhil <Home@c-50-131-63-170.hsd1.ca.comcast.net>
2013-05-20 20:28:51 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dxypherblog::Application.config.secret_token = 'eb64fcd92770fc2e4701e4e206d7b2b147bcfebcc8f4842c6729f1672bdb68edc58b859d2c166977b9cc8aa6669e97247df61d0801683878f7acbea01918a4e0'
