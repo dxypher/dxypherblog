42f77ccb76d1b067ef21249a74f761ebbeea500b
Nikhil <Home@c-50-131-63-170.hsd1.ca.comcast.net>
2013-06-03 11:20:04 -0700

    change seed file not to refer to application.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,5 @@
-puts 'ROLES'
-YAML.load(ENV['ROLES']).each do |role|
-  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
-  puts 'role: ' << role
-end
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+Role.find_or_create_by_name({ :name => 'admin' }, :without_protection => true)
+
+user = User.find_or_create_by_email :name => 'Nikhil', :email => 'dxypher1@gmail.com', :password => 'changeme', :password_confirmation => 'changeme'
+
 user.add_role :admin
