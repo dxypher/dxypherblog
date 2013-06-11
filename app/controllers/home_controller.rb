bb62192e72b9718cff2e2eb2cf7755ae8240b922
Nikhil <Home@c-50-131-63-170.hsd1.ca.comcast.net>
2013-06-10 17:43:00 -0700

    add hidden field to contact form to help weed out spam, add flash notice after contact submission.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,16 @@ class HomeController < ApplicationController
 	end
 
   def contact
-    @contact = {}
-    @contact[:name] = params[:name]
-    @contact[:email] = params[:email]
-    @contact[:message] = params[:message]
-    ContactMailer.contact_message(@contact).deliver
-    render :index
+    if params[:email] != '' # only spam bots will enter email in here.
+      redirect_to root_path
+    else
+      @contact = {}
+      @contact[:name] = params[:name]
+      @contact[:email] = params[:email_2]
+      @contact[:message] = params[:message]
+      ContactMailer.contact_message(@contact).deliver
+      flash[:notice] = "Thanks for contacting me."
+      redirect_to root_path
+    end
   end
 end
