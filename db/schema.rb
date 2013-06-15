99db0cfda7eae94812e2388ef53c2e33838afd08
Nikhil <Home@c-50-131-63-170.hsd1.ca.comcast.net>
2013-06-15 11:37:16 -0700

    add index to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130604234033) do
+ActiveRecord::Schema.define(:version => 20130615183210) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(:version => 20130604234033) do
     t.integer  "article_id"
     t.integer  "user_id"
   end
+
+  add_index "comments", ["article_id"], :name => "index_comments_on_article_id"
 
   create_table "roles", :force => true do |t|
     t.string   "name"
