31502938d0b601e56c40377049b5f5a6948a2773
Nikhil <Home@c-50-131-63-170.hsd1.ca.comcast.net>
2013-06-15 19:11:59 -0700

    add styling to article#show page and comments, add avatar to comments and avatar_url helper to get avatar from gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,9 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def avatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png"
+  end
+
 end
